2b00be8af7878fbf9a9bfb0c465591d086dbde40
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2020-10-18 00:57:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
-import "./tailwind.output.css";
+import './tailwind.output.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
