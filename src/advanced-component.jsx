ba135fba3176387afa376821dc169cc74e478c8d
Ryan Fitzgerald <me@ryanfitzgerald.ca>
2020-10-21 23:28:54 -0400

    Update advanced-component.jsx

--- src/advanced-component.jsx
+++ src/advanced-component.jsx
@@ -4,7 +4,7 @@ import React, {useState} from 'react';
 const classes = {
   wrapper: 'border-2 m-16 p-16 text-center',
   title: 'text-gray-800 text-xl font-bold',
-  description: (active) => `my-6 ${active ? 'text-red-900 font-medium' : 'text-gray-800'}`,
+  description: (active) => `my-6 ${active ? 'text-red-900' : 'text-gray-800'}`,
   button: 'py-2 px-4 bg-gray-100 border-2 focus:outline-none',
   buttonActive: 'bg-gray-400',
 };
@@ -16,7 +16,7 @@ const AdvancedComponent = () => {
     <div className={classes.wrapper}>
       <h1 className={classes.title}>Advanced Component</h1>
       <p className={classes.description(active)}>I change based on the button click state.</p>
-      <button className={clsx([classes.button, active && classes.buttonActive])} onClick={() => setActive(!active)}>Click Me</button>
+      <button className={clsx([classes.button, active && classes.buttonActive])} onClick={() => setActive((prevState) => !prevState)}>Click Me</button>
     </div>
   );
 };
